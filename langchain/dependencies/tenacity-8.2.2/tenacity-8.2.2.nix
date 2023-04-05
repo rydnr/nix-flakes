ef470624e2d42a7ba1317f01224e52003c186e06
rydnr <github@acm-sl.org>
2023-04-05 17:18:34 +0200

    Added initial tenacity-8.2.2 flake

---
+++ langchain/dependencies/tenacity-8.2.2/tenacity-8.2.2.nix
@@ -0,0 +1,31 @@
+{ lib, buildPythonPackage, fetchPypi, isPy27, isPy3k, pbr, six, futures ? null
+, monotonic ? null, typing ? null, setuptools-scm, pytest, sphinx, tornado
+, typeguard }:
+
+buildPythonPackage rec {
+  pname = "tenacity";
+  version = "8.2.2";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "";
+  };
+
+  nativeBuildInputs = [ pbr setuptools-scm ];
+  propagatedBuildInputs = [ six ]
+    ++ lib.optionals isPy27 [ futures monotonic typing ];
+
+  checkInputs = [ pytest sphinx tornado ] ++ lib.optionals isPy3k [ typeguard ];
+  checkPhase = if isPy27 then ''
+    pytest --ignore='tenacity/tests/test_asyncio.py'
+  '' else ''
+    pytest
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/jd/tenacity";
+    description = "Retrying library for Python";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ jakewaksbaum ];
+  };
+}
--- langchain/flake.nix
+++ langchain/flake.nix
@@ -4,74 +4,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
-    langchain = {
-      url = "https://github.com/hwchase17/langchain";
-      flake = false;
-    };
-    nixpkgs-pyproject = {
-      url = "github:nix-community/nixpkgs-pyproject";
-      flake = false;
-    };
-    blis-flake.url =
-      "github:rydnr/nix-flakes/langchain/dependencies/blis-0.7.9/flake";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, langchain, nixpkgs-pyproject }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pythonDependenciesOverlay = self: super: {
-          python3Packages = super.python3Packages // {
-            blis = self.blis-flake.defaultPackage.${system};
-          };
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+        langchain-src = pkgs.fetchFromGitHub {
+          owner = "hwchase17";
+          repo = "langchain";
+          rev = "v0.0.131";
+          sha256 = "sha256-qBEU/SZO8HP68QFKcBJzfiC02F91YyN4ctp9A40CvWk=";
         };
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ pythonDependenciesOverlay ];
-        };
+      in rec {
+        packages.langchain =
+          pkgs.poetry2nix.mkPoetryApplication { projectDir = langchain-src; };
+        packages.default = packages.langchain;
 
-        pyproject2nixFile = langchain:
-          with pkgs;
-          writeTextFile {
-            name = "pyproject2nix.toml";
-            text = ''
-              [tool.pyproject2nix]
-              python_version = "3.9"
-            '';
-            destination = "/pyproject2nix.toml";
-          };
-
-        langchainWithPyproject2nix = langchainSrc:
-          with pkgs;
-          runCommand "langchain-with-pyproject2nix" {
-            inherit (langchainSrc) src;
-            buildInputs = [ nixpkgs-pyproject ];
-            passAsFile = [ "pyproject2nix" ];
-          } ''
-            cp -r $src $out
-            cp $pyproject2nixPath $out/pyproject2nix.toml
-            pushd $out
-            pyproject2nix
-            popd
-          '';
-
-        langchainPackage = let
-          langchainSrc = langchain;
-          langchainWithToml = pyproject2nixFile langchainSrc;
-          langchainWithPyproject2nix = langchainWithPyproject2nix langchainSrc;
-        in import "${langchainWithPyproject2nix}/default.nix";
-
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs.python3Packages; [
-            python
-            jupyterlab
-            numpy
-            pandas
-            matplotlib
-            scipy
-            importlib-metadata
-            langchain
-            blis
+        devShells = pkgs.mkShellNoCC {
+          packages = [
+            (pkgs.${system}.poetry2nix.mkPoetryApplication {
+              projectDir = langchain-src;
+            })
+            pkgs.poetry
           ];
         };
       });
