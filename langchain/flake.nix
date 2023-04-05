7deeabf02d2cf61f5e39a64781350a48d784a1f9
rydnr <github@acm-sl.org>
2023-04-05 17:39:47 +0200

    nix develop does not complain for langchain flake

--- langchain/flake.nix
+++ langchain/flake.nix
@@ -4,32 +4,84 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    tenacity-flake.url =
+      "github:rydnr/nix-flakes?dir=langchain/dependencies/tenacity-8.2.2";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, tenacity-flake }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python3;
-        pythonPackages = python.pkgs;
-        langchain-src = pkgs.fetchFromGitHub {
-          owner = "hwchase17";
-          repo = "langchain";
-          rev = "v0.0.131";
-          sha256 = "sha256-qBEU/SZO8HP68QFKcBJzfiC02F91YyN4ctp9A40CvWk=";
+        tenacityOverlay = self: super: {
+          python3Packages = super.python3Packages // {
+            tenacity = tenacity-flake.packages.${system}.tenacity-8_2_2;
+          };
         };
-      in rec {
-        packages.langchain =
-          pkgs.poetry2nix.mkPoetryApplication { projectDir = langchain-src; };
-        packages.default = packages.langchain;
 
-        devShells = pkgs.mkShellNoCC {
-          packages = [
-            (pkgs.${system}.poetry2nix.mkPoetryApplication {
-              projectDir = langchain-src;
-            })
-            pkgs.poetry
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ tenacityOverlay ];
+        };
+
+        langchainPackage = let python = pkgs.python310;
+        in python.pkgs.buildPythonPackage {
+
+          pname = "langchain";
+          version = "0.0.131";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "hwchase17";
+            repo = "langchain";
+            rev = "v0.0.131";
+            sha256 = "sha256-qBEU/SZO8HP68QFKcBJzfiC02F91YyN4ctp9A40CvWk=";
+          };
+
+          format = "pyproject";
+
+          buildInputs = [ pkgs.poetry ];
+
+          propagatedBuildInputs = let
+            requiredDeps = with pkgs.python3Packages; [
+              pydantic
+              sqlalchemy
+              requests
+              pyyaml
+              numpy
+              blis
+            ];
+            optionalDeps = with pkgs.python3Packages; [
+              faiss
+              redis
+              spacy
+              nltk
+              beautifulsoup4
+              pytorch
+              jinja2
+              google-api-python-client
+              dataclasses-json
+              tenacity
+            ];
+          in requiredDeps ++ optionalDeps;
+
+          pythonImportsCheck = [ "langchain" ];
+          meta = with pkgs.lib; {
+            description =
+              "Building applications with LLMs through composability";
+            homepage = "https://langchain.readthedocs.io/en/latest/?";
+            license = licenses.mit;
+            maintainers = with maintainers; [ breakds ];
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.python3Packages; [
+            python
+            jupyterlab
+            numpy
+            pandas
+            matplotlib
+            scipy
+            importlib-metadata
+            langchainPackage
           ];
         };
       });
