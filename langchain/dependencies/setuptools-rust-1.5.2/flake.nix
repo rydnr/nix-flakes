5ef231773b2cbffb20ca54e1ef2ce1b7420add7b
rydnr <github@acm-sl.org>
2023-04-10 16:16:35 +0200

    Minor style changes in setuptools-rust

--- langchain/dependencies/setuptools-rust-1.5.2/flake.nix
+++ langchain/dependencies/setuptools-rust-1.5.2/flake.nix
@@ -6,23 +6,29 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs:
+    with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         python = pkgs.python3;
         pythonPackages = python.pkgs;
       in rec {
-        packages.setuptools-rust-1_5_2 = (import ./setuptools-rust-1.5.2.nix) {
-          inherit (pythonPackages)
-            buildPythonPackage fetchPypi pythonOlder semantic-version
-            typing-extensions;
-          inherit (pkgs) lib;
+        packages = {
+          setuptools-rust-1_5_2 = (import ./setuptools-rust-1.5.2.nix) {
+            inherit (pythonPackages)
+              buildPythonPackage fetchPypi pythonOlder semantic-version
+              typing-extensions;
+            inherit (pkgs) lib;
+          };
+          setuptools-rust = packages.setuptools-rust-1_5_2;
+          default = packages.setuptools-rust-1_5_2;
         };
-        packages.default = packages.setuptools-rust-1_5_2;
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs.python3Packages;
-            [ packages.setuptools-rust-1_5_2 ];
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
         };
       });
 }
