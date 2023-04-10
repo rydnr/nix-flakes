275dd4c6ca2d04589c4548bc41ad88f9ccf65b44
rydnr <github@acm-sl.org>
2023-04-10 17:29:39 +0200

    Style changes in tenacity-8.2.2

--- langchain/dependencies/tenacity-8.2.2/flake.nix
+++ langchain/dependencies/tenacity-8.2.2/flake.nix
@@ -6,22 +6,29 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs:
+    with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         python = pkgs.python3;
         pythonPackages = python.pkgs;
       in rec {
-        packages.tenacity-8_2_2 = (import ./tenacity-8.2.2.nix) {
-          inherit (pythonPackages)
-            buildPythonPackage fetchPypi isPy27 isPy3k pbr pytest six futures
-            monotonic typing setuptools-scm sphinx tornado typeguard;
-          inherit (pkgs) lib;
+        packages = {
+          tenacity-8_2_2 = (import ./tenacity-8.2.2.nix) {
+            inherit (pythonPackages)
+              buildPythonPackage fetchPypi isPy27 isPy3k pbr pytest six futures
+              monotonic typing setuptools-scm sphinx tornado typeguard;
+            inherit (pkgs) lib;
+          };
+          tenacity = packages.tenacity-8_2_2;
+          default = packages.tenacity;
         };
-        packages.default = packages.tenacity-8_2_2;
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs.python3Packages; [ packages.tenacity-8_2_2 ];
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
         };
       });
 }
