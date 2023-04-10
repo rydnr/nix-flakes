4b7f1f74f826d9db7e980ac6017ea3dfad22c318
rydnr <github@acm-sl.org>
2023-04-10 16:21:25 +0200

    Style changes in aiohttp flake

--- langchain/dependencies/aiohttp-3.8.4/flake.nix
+++ langchain/dependencies/aiohttp-3.8.4/flake.nix
@@ -6,29 +6,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs:
+    with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         python = pkgs.python3;
         pythonPackages = python.pkgs;
       in rec {
-        packages.aiohttp-3_8_4 = (import ./aiohttp-3.8.4.nix) {
-          inherit (pythonPackages)
-            buildPythonPackage fetchPypi pythonOlder
-            # install_requires
-            attrs charset-normalizer multidict async-timeout yarl frozenlist
-            aiosignal aiodns brotli cchardet asynctest typing-extensions
-            idna-ssl
-            # tests_require
-            async_generator freezegun gunicorn pytest-mock pytestCheckHook
-            re-assert trustme;
+        packages = {
+          aiohttp-3_8_4 = (import ./aiohttp-3.8.4.nix) {
+            inherit (pythonPackages)
+              buildPythonPackage fetchPypi pythonOlder
+              # install_requires
+              attrs charset-normalizer multidict async-timeout yarl frozenlist
+              aiosignal aiodns brotli cchardet asynctest typing-extensions
+              idna-ssl
+              # tests_require
+              async_generator freezegun gunicorn pytest-mock pytestCheckHook
+              re-assert trustme;
 
-          inherit (pkgs) lib stdenv;
+            inherit (pkgs) lib stdenv;
+          };
+          aiohttp = packages.aiohttp-3_8_4;
+          default = packages.aiohttp;
         };
-        packages.default = packages.aiohttp-3_8_4;
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs.python3Packages; [ packages.aiohttp-3_8_4 ];
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
         };
       });
 }
