96123f0b9d2afe0a0a43fe0618581b8e5ee9013b
rydnr <github@acm-sl.org>
2023-04-11 16:00:33 +0200

    tiktoken in progress

--- langchain/dependencies/tiktoken-0.3.3/flake.nix
+++ langchain/dependencies/tiktoken-0.3.3/flake.nix
@@ -10,6 +10,8 @@
       "github:rydnr/nix-flakes?dir=langchain/dependencies/setuptools-rust-1.5.2";
     bstr-flake.url =
       "github:rydnr/nix-flakes?dir=langchain/dependencies/bstr-1.0.1";
+    fancy-regex-flake.url =
+      "github:rydnr/nix-flakes?dir=langchain/dependencies/fancy-regex-0.10.0";
     cargo2nix.url = "github:tenx-tech/cargo2nix";
   };
 
@@ -24,6 +26,19 @@
         };
         python = pkgs.python3;
         pythonPackages = python.pkgs;
+        # create the workspace & dependencies package set
+        workspace = pkgs.fetchFromGitHub {
+          owner = "openai";
+          repo = "tiktoken";
+          rev = "0.3.3";
+          hash = "sha256-5YucJjoYAUTRy7oJ3yb2uE1UZDLIPXnV0bJIDgwGOeA=";
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.61.0";
+          packageFun = import ./Cargo.nix;
+          workspaceSrc = workspace;
+        };
       in rec {
         packages = {
           tiktoken-0_3_3 = (import ./tiktoken-0.3.3.nix) {
@@ -35,14 +50,36 @@
             blobfile = blobfile-flake.packages.${system}.blobfile;
             setuptools-rust =
               setuptools-rust-flake.packages.${system}.setuptools-rust;
-            bstr = bstr-flake.packages.${system}.bstr;
+            bstr-src = bstr-flake.packages.${system}.bstr-src;
+            fancy-regex-src =
+              fancy-regex-flake.packages.${system}.fancy-regex-src;
+            rustPkgs = rustPkgs;
             cargo2nix = cargo2nix;
           };
           tiktoken = packages.tiktoken-0_3_3;
           default = packages.tiktoken;
+          meta = with lib; {
+            description = "Fast BPE tokeniser for use with OpenAI's models.";
+            homepage = "https://github.com/openai/tiktoken";
+            license = licenses.mit;
+            maintainers = with maintainers; [ ];
+            platforms = platforms.all;
+          };
         };
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs.python3Packages; [ packages.default ];
+          buildInputs = with pkgs; [ # packages.default
+            cargo
+            rustc
+            setuptools-rust-flake.packages.${system}.setuptools-rust
+            blobfile-flake.packages.${system}.blobfile
+            pythonPackages.regex
+            pythonPackages.requests
+            pythonPackages.semantic-version
+            pythonPackages.setuptools
+            pythonPackages.typing-extensions
+            bstr-flake.packages.${system}.bstr-src
+            fancy-regex-flake.packages.${system}.fancy-regex-src
+          ];
         };
         shell = flake-utils.lib.mkShell {
           packages = system: [ self.packages.${system}.default ];
--- langchain/dependencies/tiktoken-0.3.3/tiktoken-0.3.3.nix
+++ langchain/dependencies/tiktoken-0.3.3/tiktoken-0.3.3.nix
@@ -1,36 +1,149 @@
-{ blobfile, buildPythonPackage, cargo, cargo2nix, fetchPypi, lib, regex
-, requests, rustc, semantic-version, setuptools-rust, typing-extensions }:
+{ blobfile, bstr-src, buildPythonPackage, cargo, cargo2nix, fancy-regex-src
+, fetchFromGitHub, lib, regex, requests, rustc, rustBuilder, rustPlatform
+, rustPkgs, semantic-version, setuptools-rust, typing-extensions }:
 
-buildPythonPackage rec {
+let
+  tiktoken-src = {
+    url = "github:openai/tiktoken?rev=0.3.3";
+    flake = false;
+  };
+
+  rustPackages = import ./Cargo.nix {
+    inherit (rustPlatform) cargo fetchCargoTarball;
+    inherit lib;
+    workspaceSrc = fetchFromGitHub {
+      owner = "openai";
+      repo = "tiktoken";
+      rev = "0.3.3";
+      hash = "sha256-5YucJjoYAUTRy7oJ3yb2uE1UZDLIPXnV0bJIDgwGOeA=";
+    };
+  };
+
+  tiktoken-rust = rustPackages.workspace.tiktoken { };
+in buildPythonPackage rec {
   pname = "tiktoken";
   version = "0.3.3";
   format = "setuptools";
 
-  src = let
-    src_ = fetchPypi {
-      inherit pname version;
-      hash = "sha256-l7WLe/2pRXkeyFXlPRZujsIMY3iUK5OFGmyRnd+dBJY=";
-    };
-  in src_ // { sourceRoot = "${src_.out}/source"; };
-
-  nativeBuildInputs = [ cargo rustc ];
-  propagatedBuildInputs = [
-    blobfile
-    regex
-    requests
-    semantic-version
-    setuptools-rust
-    typing-extensions
-  ];
-
-  # Add Rust dependencies from Cargo.nix
-  rustDeps = import ./Cargo.nix {
-    inherit (cargo2nix)
-      rustPackages buildRustPackages hostPlatform hostPlatformCpu
-      hostPlatformFeatures target codegenOpts profileOpts rustcLinkFlags
-      rustcBuildFlags mkRustCrate rustLib lib;
-    workspaceSrc = src.sourceRoot;
+  src = fetchFromGitHub {
+    owner = "openai";
+    repo = pname;
+    rev = version;
+    hash = "sha256-5YucJjoYAUTRy7oJ3yb2uE1UZDLIPXnV0bJIDgwGOeA=";
   };
+
+  nativeBuildInputs = [ cargo rustc setuptools-rust ];
+  #  propagatedBuildInputs = map (f: f { }.bin) (builtins.attrValues
+  #    (builtins.filter (entry: builtins.hasPrefix "registry+" entry.key)
+  #      (builtins.toList rustPkgs)));
+  propagatedBuildInputs =
+    [ blobfile regex requests semantic-version typing-extensions ] ++ [
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".aho-corasick."0.7.20"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".autocfg."1.1.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".bit-set."0.5.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".bit-vec."0.6.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".bitflags."1.3.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".bstr."1.4.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".cfg-if."1.0.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".fancy-regex."0.10.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".indoc."1.0.9"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".libc."0.2.141"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".lock_api."0.4.9"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".memchr."2.5.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".memoffset."0.6.5"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".once_cell."1.17.1"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".parking_lot."0.12.1"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".parking_lot_core."0.9.7"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".proc-macro2."1.0.56"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".pyo3."0.17.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".pyo3-build-config."0.17.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".pyo3-ffi."0.17.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".pyo3-macros."0.17.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".pyo3-macros-backend."0.17.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".quote."1.0.26"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".redox_syscall."0.2.16"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".regex."1.7.3"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".regex-automata."0.1.10"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".regex-syntax."0.6.29"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".rustc-hash."1.1.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".scopeguard."1.1.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".serde."1.0.159"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".smallvec."1.10.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".syn."1.0.109"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".target-lexicon."0.12.6"
+        { }).bin
+      (rustPkgs."unknown".tiktoken."0.3.3" { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".unicode-ident."1.0.8"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".unindent."0.1.11"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows-sys."0.45.0"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows-targets."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_aarch64_gnullvm."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_aarch64_msvc."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_i686_gnu."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_i686_msvc."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_x86_64_gnu."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_x86_64_gnullvm."0.42.2"
+        { }).bin
+      (rustPkgs."registry+https://github.com/rust-lang/crates.io-index".windows_x86_64_msvc."0.42.2"
+        { }).bin
+    ];
+
+  buildPhase = ''
+    export CARGO_HOME=$(pwd)/.cargo
+    mkdir $CARGO_HOME
+    echo '[net]' >> $(pwd)/.cargo/config
+    echo 'offline = true' >> $(pwd)/.cargo/config
+    cat $(pwd)/.cargo/config
+    runHook preBuild
+    echo "PATH: $PATH"
+    echo "NIX_BUILD_TOP: $NIX_BUILD_TOP"
+    cargo build --release
+    python setup.py build
+    runHook postBuild
+  '';
+
+  #  doBuild = false;
 
   meta = with lib; {
     description = "Fast BPE tokeniser for use with OpenAI's models.";
