3602a2752e28afe95bdc14d3d6e13d7fce4786d1
rydnr <github@acm-sl.org>
2023-06-05 21:18:18 +0200

    Added flit-scm 1.7.0

---
+++ flit-scm/flake.nix
@@ -0,0 +1,57 @@
+{
+  description =
+    "A PEP 518 build backend that uses setuptools_scm to generate a version file from your version control system, then flit_core to build the package.";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    flit-scm = {
+      url = "github:rydnr/nix-flakes/flit-scm-1.7.0?dir=flit-scm";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixos { inherit system; };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+        description =
+          "A PEP 518 build backend that uses setuptools_scm to generate a version file from your version control system, then flit_core to build the package.";
+        license = pkgs.lib.licenses.mit;
+        homepage = "https://gitlab.com/WillDaSilva/flit_scm";
+        maintainers = with pkgs.lib.maintainers; [ ];
+      in rec {
+        packages = {
+          flit-scm = pythonPackages.buildPythonPackage rec {
+            pname = "flit-scm";
+            version = "1.7.0";
+            src = pkgs.fetchgit {
+              url = "https://gitlab.com/WillDaSilva/flit_scm";
+              rev = "1.7.0";
+              sha256 = "";
+            };
+            format = "pyproject";
+
+            doCheck = false;
+
+            meta = with pkgs.lib; {
+              inherit description license homepage maintainers;
+            };
+          };
+          default = packages.flit-scm;
+          meta = with lib; {
+            inherit description license homepage maintainers;
+          };
+        };
+        defaultPackage = packages.default;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
+        };
+      });
+}
