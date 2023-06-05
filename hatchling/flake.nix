76ca246a1bb3a87d1a1ba522f7800546be5a3cec
rydnr <github@acm-sl.org>
2023-06-05 19:46:03 +0200

    Added flake for hatchling 1.17.1

--- hatchling/flake.nix
+++ hatchling/flake.nix
@@ -4,20 +4,11 @@
   inputs = rec {
     nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix/v1.28.0";
-      inputs.nixpkgs.follows = "nixos";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    python-on-nix = {
-      url = "github:on-nix/python/d8a7fa21b76ac3b8a1a3fedb41e86352769b09ed";
-      inputs.nixpkgs.follows = "nixos";
-    };
     trove-classifiers = {
-      url = "github:rydnr/nix-flakes?dir=trove-classifiers/trove-classifiers";
+      url =
+        "github:rydnr/nix-flakes/trove-classifiers-2023.5.24?dir=trove-classifiers";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
-      inputs.poetry2nix.follows = "poetry2nix";
     };
   };
   outputs = inputs:
@@ -27,7 +18,6 @@
         pkgs = import nixos { inherit system; };
         python = pkgs.python3;
         pythonPackages = python.pkgs;
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
         description = "Hatch is a modern, extensible Python project manager.";
         license = pkgs.lib.licenses.mit;
         homepage = "https://hatch.pypa.io/latest/";
@@ -37,27 +27,19 @@
           hatchling = pythonPackages.buildPythonPackage rec {
             pname = "hatchling";
             version = "1.17.1";
-            src = pkgs.fetchFromGitHub {
-              owner = "pypa";
-              repo = "hatch";
-              rev = "hatchling-v1.17.1";
-              sha256 = "sha256-OIHVEf1u/9MCDxggG9XsNLk7Z1WoEC1fOdU5zQcB0FQ=";
+            src = pythonPackages.fetchPypi {
+              pname = "hatchling";
+              version = "1.17.1";
+              sha256 = "sha256-dt7lEI+Sm365EC3wob+I+jJH1opE/x85XhzzLqqwxvo=";
             };
-            format = "backend";
+            format = "pyproject";
 
-            buildPhase = ''
-              runHook preBuild
-              ${python.interpreter} -m build backend
-              runHook postBuild
-            '';
-            nativeBuildInputs = with pythonPackages; [
-              build
-              pathspec
-              python-on-nix.packages.${system}.pluggy-latest-python39
-            ];
-            buildInputs = with pythonPackages; [
-              python-on-nix.packages.${system}.pluggy-latest-python39
-              trove-classifiers
+            nativeBuildInputs =
+              [ pythonPackages.build pythonPackages.pathspec ];
+            propagatedBuildInputs = [
+              pythonPackages.pluggy
+              pythonPackages.editables
+              trove-classifiers.packages.${system}.trove-classifiers
             ];
             doCheck = false;
 
