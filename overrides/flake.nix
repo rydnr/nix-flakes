7500c6499679de6e790b04a15be465a6a9d76dc2
rydnr <github@acm-sl.org>
2023-06-06 10:14:57 +0200

    Added overrides 7.3.1

---
+++ overrides/flake.nix
@@ -0,0 +1,49 @@
+{
+  description =
+    "A decorator @override that verifies that a method that should override an inherited method actually does it.";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixos { inherit system; };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
+        description =
+          "A decorator @override that verifies that a method that should override an inherited method actually does it.";
+        license = pkgs.lib.licenses.asl20;
+        homepage = "https://github.com/mkorpela/overrides";
+        maintainers = with pkgs.lib.maintainers; [ ];
+      in rec {
+        packages = {
+          argilla = pythonPackages.buildPythonPackage rec {
+            pname = "overrides";
+            version = "7.3.1";
+            src = pythonPackages.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-i5fGweFoG3jLyUJLE42IDwgDwiVMXrqr3eV7tsYgk/I=";
+            };
+
+            doCheck = false;
+            meta = with pkgs.lib; {
+              inherit description license homepage maintainers;
+            };
+          };
+          default = packages.argilla;
+          meta = with lib; {
+            inherit description license homepage maintainers;
+          };
+        };
+        defaultPackage = packages.default;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.python3Packages; [ packages.default ];
+        };
+        shell = flake-utils.lib.mkShell {
+          packages = system: [ self.packages.${system}.default ];
+        };
+      });
+}
