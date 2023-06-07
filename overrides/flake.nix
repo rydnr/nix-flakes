585023eebf4920653292a7dca18381aeaf22feab
rydnr <github@acm-sl.org>
2023-06-07 10:29:11 +0200

    Updated overrides flake

--- overrides/flake.nix
+++ overrides/flake.nix
@@ -3,7 +3,7 @@
     "A decorator @override that verifies that a method that should override an inherited method actually does it.";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
   outputs = { self, ... }@inputs:
@@ -20,9 +20,22 @@
           export PNAME="${package.pname}";
           export PVERSION="${package.version}";
           export PYVERSION="${python.name}";
-          export PS1="\033[37m[\[\033[01;33m\]\$PNAME-\$PVERSION\033[01;37m|\033[01;32m\]\$PYVERSION\]\033[37m|\[\033[00m\]\[\033[01;34m\]\W\033[37m]\033[31m\$\[\033[00m\] "
+          export PS1="\033[37m[\[\033[01;33m\]\$PNAME-\$PVERSION\033[01;37m|\033[01;32m\]\$PYVERSION\]\033[37m|\[\033[00m\]\[\033[01;34m\]\W\033[37m]\033[31m\$\[\033[00m\] ";
+          echo;
+          echo -e "        _           \033[34m__ _       _\033[0m";
+          echo -e "       (_)         \033[34m/ _| |     | |\033[0m";
+          echo -e "  \033[32m_ __  ___  __\033[37m   \033[34m| |_| | __ _| | _____  ___\033[0m";
+          echo -e " \033[32m| '_ \\| \\ \\/ /   \033[34m|  _| |/ _\` | |/ / _ \\/ __|\\033[0m";
+          echo -e " \033[32m| | | | |>  < \033[2m\033[46m   \033[0m\033[34m| | | | (_| |   <  __/\\__ \\ \033[0m";
+          echo -e " \033[32m|_| |_|_/_/\\_\   \033[34m|_| |_|\__,_|_|\\_\\___||___/\033[0m";
+          echo;
+          echo "Thank you for using rydnr's Nix Flakes, Nix in general, and appreciating free software!";
+          echo;
+          echo "My Nix flakes are available at https://github.com/rydnr/nix-flakes and distributed under the GPLv3 license.";
+          echo "If you want to financially reward me (although you don't have to),";
+          echo "you can do it here: https://patreon.com/rydnr";
+          echo;
         '';
-
         overrides-7_3_1-for = python:
           python.pkgs.buildPythonPackage rec {
             pname = "overrides";
