a7ed1a2f4f383525e00e855a656b6952a0d39ea0
rydnr <github@acm-sl.org>
2023-09-18 11:15:58 +0200

    Nix flake for nixpkgs' grpcio

---
+++ grpcio/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for nixpkgs' grpcio.";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixos { inherit system; };
+      in rec {
+        defaultPackage = packages.default;
+        packages = rec {
+          default = grpcio-python311;
+          grpcio-python38 = pkgs.python38.pkgs.grpcio;
+          grpcio-python39 = pkgs.python39.pkgs.grpcio;
+          grpcio-python310 = pkgs.python310.pkgs.grpcio;
+          grpcio-python311 = pkgs.python311.pkgs.grpcio;
+        };
+      });
+}
