a961ecb74e5b21fbfaaea3651b8733bdd1aa4b54
rydnr <github@acm-sl.org>
2023-09-18 11:19:34 +0200

    Nix flake for nixpkgs' requests

---
+++ requests/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for nixpkgs' requests.";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixos { inherit system; };
+      in rec {
+        defaultPackage = packages.default;
+        packages = rec {
+          default = requests-python311;
+          requests-python38 = pkgs.python38.pkgs.requests;
+          requests-python39 = pkgs.python39.pkgs.requests;
+          requests-python310 = pkgs.python310.pkgs.requests;
+          requests-python311 = pkgs.python311.pkgs.requests;
+        };
+      });
+}
