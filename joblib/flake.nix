7e567d37b2791bf8e0bf2487ab2b9435c5363c6a
rydnr <github@acm-sl.org>
2023-09-18 18:08:31 +0200

    Added nixpkgs' Python joblib

---
+++ joblib/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for nixpkgs' joblib.";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixos { inherit system; };
+      in rec {
+        defaultPackage = packages.default;
+        packages = rec {
+          default = joblib-python311;
+          joblib-python38 = pkgs.python38.pkgs.joblib;
+          joblib-python39 = pkgs.python39.pkgs.joblib;
+          joblib-python310 = pkgs.python310.pkgs.joblib;
+          joblib-python311 = pkgs.python311.pkgs.joblib;
+        };
+      });
+}
