a2b452ee57609ee218ebc44faf99a3da9e5314d8
rydnr <github@acm-sl.org>
2023-09-18 17:44:39 +0200

    Cachetools 5.3.0

---
+++ cachetools/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for nixpkgs' cachetools.";
+
+  inputs = rec {
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixos { inherit system; };
+      in rec {
+        defaultPackage = packages.default;
+        packages = rec {
+          default = cachetools-python311;
+          cachetools-python38 = pkgs.python38.pkgs.cachetools;
+          cachetools-python39 = pkgs.python39.pkgs.cachetools;
+          cachetools-python310 = pkgs.python310.pkgs.cachetools;
+          cachetools-python311 = pkgs.python311.pkgs.cachetools;
+        };
+      });
+}
