906406ae9831a1f49e692d23be465605c0cbf5ce
rydnr <github@acm-sl.org>
2023-09-29 11:49:13 +0200

    Added nixpkgs' semver

---
+++ semver/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for nixpkgs' semver.";
+
+  inputs = rec {
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixos { inherit system; };
+      in rec {
+        defaultPackage = packages.default;
+        packages = rec {
+          default = semver-python311;
+          semver-python38 = pkgs.python38.pkgs.semver;
+          semver-python39 = pkgs.python39.pkgs.semver;
+          semver-python310 = pkgs.python310.pkgs.semver;
+          semver-python311 = pkgs.python311.pkgs.semver;
+        };
+      });
+}
