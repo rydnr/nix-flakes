86767779d0fbf6455fec2b3db663d51f2ec05250
rydnr <github@acm-sl.org>
2023-09-29 11:48:06 +0200

    Added nixpkgs' paramiko

---
+++ paramiko/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for nixpkgs' paramiko.";
+
+  inputs = rec {
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixos { inherit system; };
+      in rec {
+        defaultPackage = packages.default;
+        packages = rec {
+          default = paramiko-python311;
+          paramiko-python38 = pkgs.python38.pkgs.paramiko;
+          paramiko-python39 = pkgs.python39.pkgs.paramiko;
+          paramiko-python310 = pkgs.python310.pkgs.paramiko;
+          paramiko-python311 = pkgs.python311.pkgs.paramiko;
+        };
+      });
+}
