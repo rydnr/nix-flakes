af6dc614d2a722d30200cfb13d6d414492f0f89c
rydnr <github@acm-sl.org>
2024-11-08 19:14:59 +0100

    Added azure-functions package

---
+++ azure-functions/flake.nix
@@ -0,0 +1,153 @@
+# azure-functions/flake.nix
+#
+# This file packages azure-functions as a Nix flake.
+#
+# Copyright (C) 2024-today rydnr's rydnr/nix-flakes
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+{
+  description = "Nix flake for azure-functions Python package";
+  inputs = rec {
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
+    pythoneda-shared-pythonlang-banner = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixos.follows = "nixos";
+      url = "github:pythoneda-shared-pythonlang-def/banner/0.0.56";
+    };
+    pythoneda-shared-pythonlang-domain = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixos.follows = "nixos";
+      inputs.pythoneda-shared-pythonlang-banner.follows =
+        "pythoneda-shared-pythonlang-banner";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.54";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    let
+      defaultSystems = flake-utils.lib.defaultSystems;
+      supportedSystems = if builtins.elem "armv6l-linux" defaultSystems then
+        defaultSystems
+      else
+        defaultSystems ++ [ "armv6l-linux" ];
+    in flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        version = "1.21.3";
+        sha256 = "sha256-w1m529KZjITYWV4xoo/61OjKjc+0o3mDJ/d2pn+WQ1E=";
+        org = "microsoft";
+        repo = "azure-functions";
+        pname = "azure-functions";
+        pkgs = import nixos { inherit system; };
+        description = "Azure Functions Python Library";
+        license = pkgs.lib.licenses.mit;
+        homepage = "https://www.crummy.com/software/BeautifulSoup/bs4/";
+        maintainers = with maintainers; [ domenkozar ];
+        archRole = "B";
+        space = "D";
+        layer = "D";
+        nixosVersion = builtins.readFile "${nixos}/.version";
+        nixpkgsRelease =
+          builtins.replaceStrings [ "\n" ] [ "" ] "nixos-${nixosVersion}";
+        shared = import "${pythoneda-shared-pythonlang-banner}/nix/shared.nix";
+        azure-functions-for = { python }:
+          python.pkgs.buildPythonPackage rec {
+
+            inherit pname version;
+            format = "pyproject";
+            src = python.pkgs.fetchPypi { inherit pname version sha256; };
+
+            nativeBuildInputs = with python.pkgs; [ setuptools ];
+            propagatedBuildInputs = with python.pkgs; [ requests ];
+            meta = with pkgs.lib; {
+              description = "Azure Functions Python Library";
+              license = licenses.mit;
+            };
+          };
+      in rec {
+        defaultPackage = packages.default;
+        devShells = rec {
+          default = azure-functions-default;
+          azure-functions-default = azure-functions-python311;
+          azure-functions-python38 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python38
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.azure-functions-python38;
+            python = pkgs.python38;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python38;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python38;
+            inherit archRole layer org pkgs repo space;
+          };
+          azure-functions-python39 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.azure-functions-python39;
+            python = pkgs.python39;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python39;
+            inherit archRole layer org pkgs repo space;
+          };
+          azure-functions-python310 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.azure-functions-python310;
+            python = pkgs.python310;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python310;
+            inherit archRole layer org pkgs repo space;
+          };
+          azure-functions-python311 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.azure-functions-python311;
+            python = pkgs.python311;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python311;
+            inherit archRole layer org pkgs repo space;
+          };
+        };
+        packages = rec {
+          default = azure-functions-default;
+          azure-functions-default = azure-functions-python311;
+          azure-functions-python38 =
+            azure-functions-for { python = pkgs.python38; };
+          azure-functions-python39 =
+            azure-functions-for { python = pkgs.python39; };
+          azure-functions-python310 =
+            azure-functions-for { python = pkgs.python310; };
+          azure-functions-python311 =
+            azure-functions-for { python = pkgs.python311; };
+        };
+      });
+}
