234b95c74d8c53fb7feb53aacebd1f3f5dcebdba
rydnr <github@acm-sl.org>
2024-11-16 11:02:19 +0100

    Support for Python 3.12

--- trove-classifiers/flake.nix
+++ trove-classifiers/flake.nix
@@ -2,7 +2,7 @@
   description = "Canonical source for classifiers on PyPI.";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
   outputs = inputs:
@@ -14,9 +14,9 @@
         license = pkgs.lib.licenses.asl20;
         homepage = "https://github.com/pypa/trove-classifiers";
         maintainers = with pkgs.lib.maintainers; [ ];
-        nixpkgsRelease = "nixos-23.05";
+        nixpkgsRelease = "nixos-24.05";
         shared = import ../shared.nix;
-        trove-classifiers-2023_5_24-for = python:
+        trove-classifiers-for = python:
           python.pkgs.buildPythonPackage rec {
             pname = "trove-classifiers";
             version = "2023.5.24";
@@ -36,59 +36,47 @@
         meta = with lib; { inherit description license homepage maintainers; };
 
       in rec {
-        packages = {
-          trove-classifiers-2023_5_24-python38 =
-            trove-classifiers-2023_5_24-for pkgs.python38;
-          trove-classifiers-2023_5_24-python39 =
-            trove-classifiers-2023_5_24-for pkgs.python39;
-          trove-classifiers-2023_5_24-python310 =
-            trove-classifiers-2023_5_24-for pkgs.python310;
-          trove-classifiers-2023_5_24-python311 =
-            trove-classifiers-2023_5_24-for pkgs.python311;
-          trove-classifiers-latest-python38 =
-            packages.trove-classifiers-2023_5_24-python38;
-          trove-classifiers-latest-python39 =
-            packages.trove-classifiers-2023_5_24-python39;
-          trove-classifiers-latest-python310 =
-            packages.trove-classifiers-2023_5_24-python310;
-          trove-classifiers-latest-python311 =
-            packages.trove-classifiers-2023_5_24-python311;
-          trove-classifiers-latest =
-            packages.trove-classifiers-latest-python311;
-          default = packages.trove-classifiers-latest;
-        };
         defaultPackage = packages.default;
         devShells = rec {
-          trove-classifiers-2023_5_24-python38 = shared.devShell-for {
-            package = packages.trove-classifiers-2023_5_24-python38;
+          trove-classifiers-python38 = shared.devShell-for {
+            package = packages.trove-classifiers-python38;
             python = pkgs.python38;
             inherit pkgs nixpkgsRelease;
           };
-          trove-classifiers-2023_5_24-python39 = shared.devShell-for {
-            package = packages.trove-classifiers-2023_5_24-python39;
+          trove-classifiers-python39 = shared.devShell-for {
+            package = packages.trove-classifiers-python39;
             python = pkgs.python39;
             inherit pkgs nixpkgsRelease;
           };
-          trove-classifiers-2023_5_24-python310 = shared.devShell-for {
-            package = packages.trove-classifiers-2023_5_24-python310;
+          trove-classifiers-python310 = shared.devShell-for {
+            package = packages.trove-classifiers-python310;
             python = pkgs.python310;
             inherit pkgs nixpkgsRelease;
           };
-          trove-classifiers-2023_5_24-python311 = shared.devShell-for {
-            package = packages.trove-classifiers-2023_5_24-python311;
+          trove-classifiers-python311 = shared.devShell-for {
+            package = packages.trove-classifiers-python311;
             python = pkgs.python311;
             inherit pkgs nixpkgsRelease;
           };
-          trove-classifiers-latest-python38 =
-            trove-classifiers-2023_5_24-python38;
-          trove-classifiers-latest-python39 =
-            trove-classifiers-2023_5_24-python39;
-          trove-classifiers-latest-python310 =
-            trove-classifiers-2023_5_24-python310;
-          trove-classifiers-latest-python311 =
-            trove-classifiers-2023_5_24-python311;
-          trove-classifiers-latest = trove-classifiers-latest-python311;
-          default = trove-classifiers-latest;
+          trove-classifiers-python312 = shared.devShell-for {
+            package = packages.trove-classifiers-python312;
+            python = pkgs.python312;
+            inherit pkgs nixpkgsRelease;
+          };
+          default = trove-classifiers-python312;
+        };
+        packages = {
+          default = packages.trove-classifiers-python312;
+          trove-classifiers-python38 =
+            trove-classifiers-for pkgs.python38;
+          trove-classifiers-python39 =
+            trove-classifiers-for pkgs.python39;
+          trove-classifiers-python310 =
+            trove-classifiers-for pkgs.python310;
+          trove-classifiers-python311 =
+            trove-classifiers-for pkgs.python311;
+          trove-classifiers-python312 =
+            trove-classifiers-for pkgs.python312;
         };
       });
 }
