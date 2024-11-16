db66555baef950f0b6e663dd8489586811b7a38b
rydnr <github@acm-sl.org>
2024-11-16 09:43:54 +0100

    Support for Python 3.12

--- joblib/flake.nix
+++ joblib/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for nixpkgs' joblib.";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
   outputs = inputs:
@@ -12,11 +12,12 @@
       in rec {
         defaultPackage = packages.default;
         packages = rec {
-          default = joblib-python311;
+          default = joblib-python312;
           joblib-python38 = pkgs.python38.pkgs.joblib;
           joblib-python39 = pkgs.python39.pkgs.joblib;
           joblib-python310 = pkgs.python310.pkgs.joblib;
           joblib-python311 = pkgs.python311.pkgs.joblib;
+          joblib-python312 = pkgs.python312.pkgs.joblib;
         };
       });
 }
