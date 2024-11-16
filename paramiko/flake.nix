d55b825029df1d401498587c55b2e45b7d8fcf2f
rydnr <github@acm-sl.org>
2024-11-16 09:52:38 +0100

    Support for Python 3.12

--- paramiko/flake.nix
+++ paramiko/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
   };
   outputs = inputs:
     with inputs;
@@ -12,11 +12,12 @@
       in rec {
         defaultPackage = packages.default;
         packages = rec {
-          default = paramiko-python311;
+          default = paramiko-python312;
           paramiko-python38 = pkgs.python38.pkgs.paramiko;
           paramiko-python39 = pkgs.python39.pkgs.paramiko;
           paramiko-python310 = pkgs.python310.pkgs.paramiko;
           paramiko-python311 = pkgs.python311.pkgs.paramiko;
+          paramiko-python312 = pkgs.python312.pkgs.paramiko;
         };
       });
 }
