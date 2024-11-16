e651b4d8d59876d0af4fee68d4bd5aac2bcf7c98
rydnr <github@acm-sl.org>
2024-11-16 09:51:44 +0100

    Support for Python 3.12

--- overrides/flake.nix
+++ overrides/flake.nix
@@ -3,7 +3,7 @@
     "A decorator @override that verifies that a method that should override an inherited method actually does it.";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
   outputs = { self, ... }@inputs:
@@ -18,7 +18,7 @@
         maintainers = with pkgs.lib.maintainers; [ ];
         nixpkgsRelease = "nixos-23.05";
         shared = import ../shared.nix;
-        overrides-7_3_1-for = python:
+        overrides-for = python:
           python.pkgs.buildPythonPackage rec {
             pname = "overrides";
             version = "7.3.1";
@@ -33,46 +33,42 @@
             };
           };
       in rec {
-        packages = rec {
-          overrides-7_3_1-python38 = overrides-7_3_1-for pkgs.python38;
-          overrides-7_3_1-python39 = overrides-7_3_1-for pkgs.python39;
-          overrides-7_3_1-python310 = overrides-7_3_1-for pkgs.python310;
-          overrides-7_3_1-python311 = overrides-7_3_1-for pkgs.python311;
-          overrides-latest-python38 = overrides-7_3_1-python38;
-          overrides-latest-python39 = overrides-7_3_1-python39;
-          overrides-latest-python310 = overrides-7_3_1-python310;
-          overrides-latest-python311 = overrides-7_3_1-python311;
-          overrides-latest = overrides-latest-python311;
-          default = overrides-latest;
-        };
         defaultPackage = packages.default;
         devShells = rec {
-          overrides-7_3_1-python38 = shared.devShell-for {
-            package = packages.overrides-7_3_1-python38;
+          default = overrides-python312;
+          overrides-python38 = shared.devShell-for {
+            package = packages.overrides-python38;
             python = pkgs.python38;
             inherit pkgs nixpkgsRelease;
           };
-          overrides-7_3_1-python39 = shared.devShell-for {
-            package = packages.overrides-7_3_1-python39;
+          overrides-python39 = shared.devShell-for {
+            package = packages.overrides-python39;
             python = pkgs.python39;
             inherit pkgs nixpkgsRelease;
           };
-          overrides-7_3_1-python310 = shared.devShell-for {
-            package = packages.overrides-7_3_1-python310;
+          overrides-python310 = shared.devShell-for {
+            package = packages.overrides-python310;
             python = pkgs.python310;
             inherit pkgs nixpkgsRelease;
           };
-          overrides-7_3_1-python311 = shared.devShell-for {
-            package = packages.overrides-7_3_1-python311;
+          overrides-python311 = shared.devShell-for {
+            package = packages.overrides-python311;
             python = pkgs.python311;
             inherit pkgs nixpkgsRelease;
           };
-          overrides-latest-python38 = overrides-7_3_1-python38;
-          overrides-latest-python39 = overrides-7_3_1-python39;
-          overrides-latest-python310 = overrides-7_3_1-python310;
-          overrides-latest-python311 = overrides-7_3_1-python311;
-          overrides-latest = overrides-latest-python311;
-          default = overrides-latest;
+          overrides-python312 = shared.devShell-for {
+            package = packages.overrides-python312;
+            python = pkgs.python312;
+            inherit pkgs nixpkgsRelease;
+          };
+        };
+        packages = rec {
+          default = overrides-python312;
+          overrides-python38 = overrides-for pkgs.python38;
+          overrides-python39 = overrides-for pkgs.python39;
+          overrides-python310 = overrides-for pkgs.python310;
+          overrides-python311 = overrides-for pkgs.python311;
+          overrides-python312 = overrides-for pkgs.python312;
         };
       });
 }
