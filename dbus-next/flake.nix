17ce55136228ce8fdf19785985e5b3bc8dd699e2
rydnr <github@acm-sl.org>
2024-11-16 09:38:13 +0100

    Support for Python 3.12

--- dbus-next/flake.nix
+++ dbus-next/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for nixpkgs' dbus-next.";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
   outputs = inputs:
@@ -12,11 +12,12 @@
       in rec {
         defaultPackage = packages.default;
         packages = rec {
-          default = dbus-next-python311;
+          default = dbus-next-python312;
           dbus-next-python38 = pkgs.python38.pkgs.dbus-next;
           dbus-next-python39 = pkgs.python39.pkgs.dbus-next;
           dbus-next-python310 = pkgs.python310.pkgs.dbus-next;
           dbus-next-python311 = pkgs.python311.pkgs.dbus-next;
+          dbus-next-python312 = pkgs.python312.pkgs.dbus-next;
         };
       });
 }
