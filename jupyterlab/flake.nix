d4ad35d8894f711f527b193b9f3dfcc8c3d4c848
rydnr <github@acm-sl.org>
2024-11-16 09:44:54 +0100

    Support for Python 3.12

--- jupyterlab/flake.nix
+++ jupyterlab/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for nixpkgs' jupyterlab.";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
   outputs = inputs:
@@ -12,11 +12,12 @@
       in rec {
         defaultPackage = packages.default;
         packages = rec {
-          default = jupyterlab-python311;
+          default = jupyterlab-python312;
           jupyterlab-python38 = pkgs.python38.pkgs.jupyterlab;
           jupyterlab-python39 = pkgs.python39.pkgs.jupyterlab;
           jupyterlab-python310 = pkgs.python310.pkgs.jupyterlab;
           jupyterlab-python311 = pkgs.python311.pkgs.jupyterlab;
+          jupyterlab-python312 = pkgs.python312.pkgs.jupyterlab;
         };
       });
 }
