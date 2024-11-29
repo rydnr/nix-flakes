f462dd444fafc93167a595e26ca4c126d1fb51bf
rydnr <github@acm-sl.org>
2024-11-29 19:42:03 +0100

    Removed Python3.8, added Python3.13

--- requests/flake.nix
+++ requests/flake.nix
@@ -9,15 +9,44 @@
     with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixos { inherit system; };
+          shared = import ../shared.nix;
       in rec {
         defaultPackage = packages.default;
+        devShells = rec {
+          default = requests-python312;
+          requests-python39 = shared.devShell-for {
+            package = packages.requests-python39;
+            python = pkgs.python39;
+            inherit pkgs nixpkgsRelease;
+          };
+          requests-python310 = shared.devShell-for {
+            package = packages.requests-python310;
+            python = pkgs.python310;
+            inherit pkgs nixpkgsRelease;
+          };
+          requests-python311 = shared.devShell-for {
+            package = packages.requests-python311;
+            python = pkgs.python311;
+            inherit pkgs nixpkgsRelease;
+          };
+          requests-python312 = shared.devShell-for {
+            package = packages.requests-python312;
+            python = pkgs.python312;
+            inherit pkgs nixpkgsRelease;
+          };
+          requests-python313 = shared.devShell-for {
+            package = packages.requests-python313;
+            python = pkgs.python313;
+            inherit pkgs nixpkgsRelease;
+          };
+        };
         packages = rec {
           default = requests-python312;
-          requests-python38 = pkgs.python38.pkgs.requests;
           requests-python39 = pkgs.python39.pkgs.requests;
           requests-python310 = pkgs.python310.pkgs.requests;
           requests-python311 = pkgs.python311.pkgs.requests;
           requests-python312 = pkgs.python312.pkgs.requests;
+          requests-python313 = pkgs.python313.pkgs.requests;
         };
       });
 }
