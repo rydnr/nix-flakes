42fd57e3e99276013b04814aaf679528f6b350e0
rydnr <github@acm-sl.org>
2024-11-29 20:21:02 +0100

    Removed Python3.8, added Python3.13

--- argilla/flake.nix
+++ argilla/flake.nix
@@ -1,14 +1,14 @@
 {
-  description = "Open-source data curation platform for LLMs";
+  description = "Nix flake for argilla";
 
   inputs = rec {
-    nixos.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     flit = {
-      url = "github:rydnr/nix-flakes/flit-3.9.0?dir=flit";
+      url = "github:rydnr/nix-flakes/flit-3.9.0.1?dir=flit";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
     };
+    nixos.url = "github:NixOS/nixpkgs/24.05";
   };
   outputs = inputs:
     with inputs;
@@ -24,61 +24,52 @@
           export PVERSION="${package.version}";
           export PS1="\[\033[01;32m\][\$PNAME-\$PVERSION]\[\033[00m\] \[\033[01;34m\]\W \$\[\033[00m\] "
         '';
-        argilla-1_8_0-for = python:
+        argilla-for = { flit-core, python }:
           python.pkgs.buildPythonPackage rec {
             pname = "argilla";
-            version = "1.8.0";
+            version = "2.4.0";
             src = python.pkgs.fetchPypi {
               inherit pname version;
-              sha256 = "sha256-HXh6ZmZc9EAxTaxGdRFM/l80YroBm0nY59SHxKBF/FQ=";
+              sha256 = "sha256-E+MdHaqpA24ydo8bUnsdvwjOvzZdHg+a9lQNtsu7ftQ=";
             };
             format = "pyproject";
 
             nativeBuildInputs =
-              [ flit.packages.${system}.flit-core python.pkgs.setuptools ];
+              [ flit-core python.pkgs.setuptools ];
 
+            propagatedBuildInputs = with python.pkgs; [ backoff deprecated httpx monotonic numpy pandas pydantic tqdm typer wrapt ];
             meta = with pkgs.lib; {
               inherit description license homepage maintainers;
             };
           };
 
       in rec {
-        packages = {
-          argilla-1_8_0-python37 = argilla-1_8_0-for pkgs.python37;
-          argilla-1_8_0-python38 = argilla-1_8_0-for pkgs.python38;
-          argilla-1_8_0-python39 = argilla-1_8_0-for pkgs.python39;
-          argilla-1_8_0-python310 = argilla-1_8_0-for pkgs.python310;
-          latest-python37 = packages.argilla-1_8_0-python37;
-          latest-python38 = packages.argilla-1_8_0-python38;
-          latest-python39 = packages.argilla-1_8_0-python39;
-          latest-python310 = packages.argilla-1_8_0-python310;
-          latest = packages.latest-python310;
-          default = packages.latest;
-        };
         defaultPackage = packages.default;
         devShells = {
-          argilla-1_8_0-python37 = pkgs.mkShell {
-            buildInputs = [ packages.argilla-1_8_0-python37 ];
-            shellHook = shellHook-for packages.latest-python37;
-          };
-          argilla-1_8_0-python38 = pkgs.mkShell {
-            buildInputs = [ packages.argilla-1_8_0-python38 ];
-            shellHook = shellHook-for packages.latest-python38;
-          };
-          argilla-1_8_0-python39 = pkgs.mkShell {
-            buildInputs = [ packages.argilla-1_8_0-python39 ];
-            shellHook = shellHook-for packages.latest-python39;
-          };
-          argilla-1_8_0-python310 = pkgs.mkShell {
-            buildInputs = [ packages.argilla-1_8_0-python310 ];
+          argilla-python310 = pkgs.mkShell {
+            buildInputs = [ packages.argilla-python310 ];
             shellHook = shellHook-for packages.latest-python310;
           };
-          latest-python37 = devShells.argilla-1_8_0-python37;
-          latest-python38 = devShells.argilla-1_8_0-python38;
-          latest-python39 = devShells.argilla-1_8_0-python39;
-          latest-python310 = devShells.argilla-1_8_0-python310;
-          latest = devShells.latest-python310;
-          default = devShells.latest;
+          argilla-python311 = pkgs.mkShell {
+            buildInputs = [ packages.argilla-python311 ];
+            shellHook = shellHook-for packages.latest-python311;
+          };
+          argilla-python312 = pkgs.mkShell {
+            buildInputs = [ packages.argilla-python312 ];
+            shellHook = shellHook-for packages.latest-python312;
+          };
+          argilla-python313 = pkgs.mkShell {
+            buildInputs = [ packages.argilla-python313 ];
+            shellHook = shellHook-for packages.latest-python313;
+          };
+          default = devShells.argilla-python312;
+        };
+        packages = {
+          argilla-python310 = argilla-for { flit-core = flit.packages.${system}.flit-core-python310; python = pkgs.python310; };
+          argilla-python311 = argilla-for { flit-core = flit.packages.${system}.flit-core-python311; python = pkgs.python311; };
+          argilla-python312 = argilla-for { flit-core = flit.packages.${system}.flit-core-python312; python = pkgs.python312; };
+          argilla-python313 = argilla-for { flit-core = flit.packages.${system}.flit-core-python313; python = pkgs.python313; };
+          default = packages.argilla-python312;
         };
       });
 }
