5401036ce443c6bd9382c897500b01a16967ac2f
rydnr <github@acm-sl.org>
2024-11-29 18:51:36 +0100

    Removed Python3.8, added Python3.13

--- GitPython/flake.nix
+++ GitPython/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake for nixpkgs' GitPython.";
+  description = "Flake for nixpkgs' GitPython";
 
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
@@ -13,11 +13,11 @@
         defaultPackage = packages.default;
         packages = rec {
           default = GitPython-python312;
-          GitPython-python38 = pkgs.python38.pkgs.GitPython;
           GitPython-python39 = pkgs.python39.pkgs.GitPython;
           GitPython-python310 = pkgs.python310.pkgs.GitPython;
           GitPython-python311 = pkgs.python311.pkgs.GitPython;
           GitPython-python312 = pkgs.python312.pkgs.GitPython;
+          GitPython-python313 = pkgs.python313.pkgs.GitPython;
         };
       });
 }
