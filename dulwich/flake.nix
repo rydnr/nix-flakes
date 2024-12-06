60e7e23c8db80d365b4d9a2bb671c722615b88f4
rydnr <github@acm-sl.org>
2024-12-06 18:49:40 +0100

    Renamed nixos to nixpkgs

--- dulwich/flake.nix
+++ dulwich/flake.nix
@@ -3,13 +3,13 @@
 
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
   };
   outputs = inputs:
     with inputs;
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixos { inherit system; };
-          nixpkgsRelease = "nixos-24.05";
+      let pkgs = import nixpkgs { inherit system; };
+          nixpkgsRelease = "nixpkgs-24.05";
           shared = import ../shared;
       in rec {
         defaultPackage = packages.default;
