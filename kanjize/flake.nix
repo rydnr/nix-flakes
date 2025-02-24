6aec86bc06e2ff32c2c79950eb53ae4df847caac
rydnr <github@acm-sl.org>
2025-02-24 06:57:51 +0100

    Implemented kanjize-1.6.0 flake

---
+++ kanjize/flake.nix
@@ -0,0 +1,193 @@
+# kanjize/flake.nix
+#
+# This file packages kanjize as a Nix flake.
+#
+# Copyright (C) 2024-today rydnr's rydnr/nix-flakes
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+{
+  description = "Nix flake for nagataaas/Kanjize package";
+  inputs = rec {
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    pythoneda-shared-pythonlang-banner = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:pythoneda-shared-pythonlang-def/banner/0.0.72";
+    };
+    pythoneda-shared-pythonlang-domain = {
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pythoneda-shared-pythonlang-banner.follows =
+        "pythoneda-shared-pythonlang-banner";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.94";
+    };
+  };
+  outputs = inputs:
+    with inputs;
+    let
+      defaultSystems = flake-utils.lib.defaultSystems;
+      supportedSystems = if builtins.elem "armv6l-linux" defaultSystems then
+        defaultSystems
+      else
+        defaultSystems ++ [ "armv6l-linux" ];
+    in flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        org = "nagataaaas";
+        repo = "Kanjize";
+        version = "1.6.0";
+        sha256 = "";
+        pname = "kanjize";
+        pkgs = import nixpkgs { inherit system; };
+        description = "Easy converter between Kanki-Number and Integer";
+        license = pkgs.lib.licenses.mit;
+        homepage = "https://github.com/nagataaaas/kanjize";
+        maintainers = with maintainers; [ ];
+        archRole = "B";
+        space = "D";
+        layer = "D";
+        nixpkgsVersion = builtins.readFile "${nixpkgs}/.version";
+        nixpkgsRelease =
+          builtins.replaceStrings [ "\n" ] [ "" ] "nixpkgs-${nixpkgsVersion}";
+        shared = import "${pythoneda-shared-pythonlang-banner}/nix/shared.nix";
+        kanjize-for = { python }:
+          let
+            pnameWithUnderscores =
+              builtins.replaceStrings [ "-" ] [ "_" ] pname;
+            pythonVersionParts = builtins.splitVersion python.version;
+            pythonMajorVersion = builtins.head pythonVersionParts;
+            pythonMajorMinorVersion =
+              "${pythonMajorVersion}.${builtins.elemAt pythonVersionParts 1}";
+            wheelName =
+              "${pnameWithUnderscores}-${version}-py${pythonMajorVersion}-none-any.whl";
+          in python.pkgs.buildPythonPackage rec {
+
+            inherit pname version;
+            format = "pyproject";
+            src = python.pkgs.fetchPypi { inherit pname version sha256; };
+
+            nativeBuildInputs = with python.pkgs; [ pip poetry-core ];
+            propagatedBuildInputs = with python.pkgs; [
+              gradio
+              huggingface-hub
+              inflect
+              kanjize
+              numpy
+              phonemizer
+              sudachidct-full
+              sudachipy
+              soundfile
+              torch
+              torchaudio
+            ];
+            postInstall = ''
+              command mkdir -p $out/dist
+              command cp dist/${wheelName} $out/dist
+              '';
+            meta = with pkgs.lib; {
+              description = "Leading open-weight text-to-speech model";
+              license = licenses.mit;
+            };
+          };
+      in rec {
+        defaultPackage = packages.default;
+        devShells = rec {
+          default = zonos-python312;
+          zonos-python39 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.zonos-python39;
+            python = pkgs.python39;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python39;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python39;
+            inherit archRole layer org pkgs repo space;
+          };
+          zonos-python310 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.zonos-python310;
+            python = pkgs.python310;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python310;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python310;
+            inherit archRole layer org pkgs repo space;
+          };
+          zonos-python311 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.zonos-python311;
+            python = pkgs.python311;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python311;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python311;
+            inherit archRole layer org pkgs repo space;
+          };
+          zonos-python312 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.zonos-python312;
+            python = pkgs.python312;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python312;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python312;
+            inherit archRole layer org pkgs repo space;
+          };
+          zonos-python313 = shared.devShell-for {
+            banner = "${
+                pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python313
+              }/bin/banner.sh";
+            extra-namespaces = "";
+            nixpkgs-release = nixpkgsRelease;
+            package = packages.zonos-python313;
+            python = pkgs.python313;
+            pythoneda-shared-pythonlang-banner =
+              pythoneda-shared-pythonlang-banner.packages.${system}.pythoneda-shared-pythonlang-banner-python313;
+            pythoneda-shared-pythonlang-domain =
+              pythoneda-shared-pythonlang-domain.packages.${system}.pythoneda-shared-pythonlang-domain-python313;
+            inherit archRole layer org pkgs repo space;
+          };
+        };
+        packages = rec {
+          default = zonos-default;
+          zonos-default = zonos-python312;
+          zonos-python39 =
+            kanjize-for { python = pkgs.python39; };
+          zonos-python310 =
+            kanjize-for { python = pkgs.python310; };
+          zonos-python311 =
+            kanjize-for { python = pkgs.python311; };
+          zonos-python312 =
+            kanjize-for { python = pkgs.python312; };
+          zonos-python313 =
+            kanjize-for { python = pkgs.python313; };
+        };
+      });
+}
